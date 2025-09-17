1836a947f383f6d7003c9a56500bc69e161ae6c1
luifdo <luifsanchez@gmail.cpm>
2025-09-16 19:43:34 -0500

    creacion del test y uso del repositorio

--- rentCart/src/test/java/unimag/edu/pw/domine/repository/VehicleRepositoryTest.java
+++ rentCart/src/test/java/unimag/edu/pw/domine/repository/VehicleRepositoryTest.java
@@ -1,14 +1,42 @@
 package unimag.edu.pw.domine.repository;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import unimag.edu.pw.domine.entities.Vehicle;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
-
+@Testcontainers
 class VehicleRepositoryTest {
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(dockerImageName: "postgress:: id")
+
+    @Autowired
+    VehicleRepository vehicleRepository;
+    private Vehicle vehicle;
 
     @BeforeEach
     void setUp() {
+    }
+
+
+    @Test
+    @DisplayName("Dado los datos de un vehicle guardarlo en la db")
+    void save_vehicle() {
+        // arrange
+        //atc
+        vehicleRepository.save(vehicle);
+        //Assert
+        assertThat(vehicle.getId()).isNotNull();
     }
 }
