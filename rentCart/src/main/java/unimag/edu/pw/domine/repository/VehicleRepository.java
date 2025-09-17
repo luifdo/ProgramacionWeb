23fa95a41621e9332d0583794fb907b0f5833008
luifdo <luifsanchez@gmail.cpm>
2025-09-16 19:25:30 -0500

    Coloque las especificaciones de para que funcione el test contanier

--- rentCart/src/main/java/unimag/edu/pw/domine/repository/VehicleRepository.java
+++ rentCart/src/main/java/unimag/edu/pw/domine/repository/VehicleRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import unimag.edu.pw.domine.entities.Vehicle;
 
 public interface VehicleRepository extends JpaRepository<Vehicle,Long> {
+
 }
---
+++ rentCart/src/test/java/unimag/edu/pw/domine/repository/VehicleRepositoryTest.java
@@ -0,0 +1,14 @@
+package unimag.edu.pw.domine.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+
+class VehicleRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
